3dbae5cafeb75e37109b98fd3df6330e82f34f44
João Daniel Queiroz da Silva <daniel.queiroz@academico.ifrn.edu.br>
2021-07-05 21:53:07 -0300

    Correção na sintaxe do Doxygen

--- include/Arquivo.hpp
+++ include/Arquivo.hpp
@@ -11,14 +11,14 @@ class Arquivo{
         string local;
     public:
         
-        /*
+        /*!
           @brief Seta a variavel 'local' do objeto que foi instanciado
           @param local_ Local onde está o arquivo.
         */
         Arquivo(string local_);
 
-         /*
-          @brief Lê e separa as linhas do arquivo e verifica se houve algúm erro de diretório.
+         /*!
+          @brief Lê e separa as linhas do arquivo e verifica se houve algum erro de diretório.
           @param n Número de linhas a serem lidas.
           @param linha Vetor de string que armazena as linhas do arquivo.
           @return Retornar True caso não houver falhas e False caso haja.
